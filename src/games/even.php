29bdddc67a94a43b652914335fe1496f8070c7e6
tungatarovM <tungatarov.96@mail.ru>
2018-09-27 11:43:02 +0500

    add brain-even

---
+++ src/games/even.php
@@ -0,0 +1,31 @@
+<?php
+
+namespace BrainGames\Games\Even;
+
+use function \BrainGames\Cli\startEngine;
+
+const EVEN_GAME_RULES = 'Answer "yes" if number even otherwise answer "no"';
+
+function getCorrectAnswer($question)
+{
+    $answer = isEven($question) ? 'yes' : 'no';
+    return $answer;
+}
+
+function isEven($num)
+{
+    return $num % 2 === 0;
+}
+
+function gameRun()
+{
+    $gameData = function () {
+        $question = rand(0, 100);
+        $answer = getCorrectAnswer($question);
+        return [
+            "getQuestion" => $question,
+            "getCorrectAnswer" => "{$answer}"
+        ];
+    };
+    return startEngine($gameData, EVEN_GAME_RULES);
+}
